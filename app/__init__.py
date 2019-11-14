48de77c3f9151e43a07d0b05d3824cc87225c6f4
marprin <marprin93@gmail.com>
2019-11-14 11:22:33 +0700

    Add hiredis lib Encapsulate redis and werkzeug as one package update /health endpoint to check cache as well

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,11 @@
 import logging.config
 import settings
-from flask import Flask, jsonify
+from common.cache import Redis
+from flask import Flask, jsonify, current_app
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
-from werkzeug.contrib.cache import RedisCache
-from werkzeug.utils import import_string
 from route.route import register_route_blueprint
+from werkzeug.utils import import_string
 
 
 def create_app():
@@ -13,13 +13,12 @@ def create_app():
     app = Flask(__name__, instance_relative_config=True)
 
     app.config.from_object(settings)
-
-    app.cache = RedisCache(
+    app.cache = Redis(
         host=settings.REDIS_HOST,
         port=settings.REDIS_PORT,
         db=settings.REDIS_DB,
         password=settings.REDIS_PASSWORD,
-    )
+    ).get_connection()
 
     Limiter(app, key_func=get_remote_address, default_limits=["50 per minute"])
 
@@ -33,4 +32,4 @@ app = create_app()
 
 @app.route("/health")
 def health_check():
-    return jsonify({"status": "ok"}), 200
+    return jsonify({"status": "ok", "cache": current_app.cache.ping()}), 200
--- settings.py
+++ settings.py
@@ -23,10 +23,11 @@ DB_PORT = os.environ.get("DB_PORT", 3306)
 DB_HOST = os.environ.get("DB_HOST", "localhost")
 DB_NAME = os.environ.get("DB_NAME", "flask_rq")
 
+REDIS_URL = os.environ.get("REDIS_URL")
 REDIS_HOST = os.environ.get("REDIS_HOST", "localhost")
 REDIS_PORT = os.environ.get("REDIS_PORT", 6379)
 REDIS_DB = os.environ.get("REDIS_DB", 0)
-REDIS_PASSWORD = os.environ.get("REDIS_PASSWORD")
+REDIS_PASSWORD = os.environ.get("REDIS_PASSWORD", None)
 
 LOGGING_CONFIG = {
     "version": 1,
